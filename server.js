b7b3cacd6721287a0d9f8e77af276f7a06157a11
Dan Collins <dan.collins.dev@gmail.com>
2026-03-10 19:35:45 -0400

    End of POST implementation

--- server.js
+++ server.js
@@ -30,6 +30,11 @@ const port = 8080;
 */
 app.use(cors());
 
+// Necessary middleware that tells express how to
+// read the body of an incoming request.
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 // Constructing our filepath to be cross-platform friendly
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -48,10 +53,35 @@ async function getUsers() {
 // Helper function to save our user data
 async function saveUsers(users) {
     try {
-        await fs.writeFile(logsFilePath, JSON.stringify(users));
+        await fs.writeFile(usersFilePath, JSON.stringify(users));
     } catch (error) {
         console.error(error.message);
     }
+}
+
+// Helper function to create and return a
+// simple id based on the sum of all the
+// existing user ids
+async function createUserId() {
+    const users = await getUsers();
+    const userIds = users.map((user) => user.id);
+
+    return Math.max(...userIds) + 1;
+}
+
+// A factory function to easily create our users.
+// We are going to pass in the body of our request
+// as an argument
+async function createUser(reqBody) {
+    return {
+        id: await createUserId(),
+        firstName: reqBody.firstName,
+        lastName: reqBody.lastName,
+        company: reqBody.company,
+        email: reqBody.email,
+        isActive: reqBody.isActive,
+        department: reqBody.department
+    };
 }
 
 // GET - returns all users
@@ -91,6 +121,50 @@ app.get("/api/users/{:id}", async (req, res) => {
 });
 
 // POST
+app.post("/api/users", async (req, res) => {
+    if (!req.body) {
+        return res.status(400).json("Missing body");
+    }
+
+    // We are going to require all fields for our POST
+    const requiredFields = [
+        "firstName",
+        "lastName",
+        "company",
+        "email",
+        "isActive",
+        "department",
+    ];
+
+    // Here we're iterating over the required
+    // fields and making sure that they are
+    // in the body of the request.
+    const missingField = requiredFields.find(
+        (field) =>
+            req.body[field] === undefined ||
+            req.body[field] === null ||
+            req.body[field] === "",
+    );
+
+    // If a POST request is missing a field
+    // we're going to return the first occuring
+    // field that is missing.
+    if (missingField) {
+        return res.status(400).json(`${missingField} is required`);
+    }
+
+    try {
+        const users = await getUsers();
+        const newUser = await createUser(req.body)
+        
+        users.push(newUser);
+        await saveUsers(users);
+        
+        res.status(201).json(newUser);
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 // PUT
 
