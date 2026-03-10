f9d315c61363ed2da80003220a636cf1b5d8d6ef
Dan Collins <dan.collins.dev@gmail.com>
2026-03-10 08:35:08 -0400

    End of GET implementation branch

--- server.js
+++ server.js
@@ -4,10 +4,31 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 import fs from "fs/promises";
-import { v4 } from "uuid";
+import cors from "cors";
 
 const app = express();
 const port = 8080;
+
+/*
+    Middleware used to enable and configure 
+    Cross-Origin Resource Sharing (CORS). As is,
+    anyone can make calls to our API. We can pass
+    an object as a paramter to cors() which has
+    settings that we can define.
+    
+    For instance:
+
+    corsOptions = {
+        origin: "http://localhost:8080"
+    }
+
+    app.use(cors(corsOptions))
+
+    This sets the API to only allow requests from this
+    domain. There are other available congifuration options
+    which I recommend researching.
+*/
+app.use(cors());
 
 // Constructing our filepath to be cross-platform friendly
 const __filename = fileURLToPath(import.meta.url);
@@ -20,18 +41,50 @@ async function getUsers() {
         const userData = await fs.readFile(usersFilePath);
         return JSON.parse(userData);
     } catch (error) {
-        console.error(error.message);
+        console.error(error);
     }
 }
 
 async function saveUsers(users) {
-    await fs.writeFile(logsFilePath, JSON.stringify(users));
+    try {
+        await fs.writeFile(logsFilePath, JSON.stringify(users));
+    } catch (error) {
+        console.error(error.message);
+    }
 }
 
 // GET
 app.get("/api/users", async (req, res) => {
-    const users = await getUsers();
-    res.status(200).json(users);
+    try {
+        const users = await getUsers();
+        res.status(200).json(users);
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+// GET BY ID
+app.get("/api/users/{:id}", async (req, res) => {
+    if (!req.params.id) {
+        return res.status(400).json("Bad Request");
+    }
+
+    try {
+        const id = parseInt(req.params.id);
+
+        const users = await getUsers();
+        const user = users.find((user) => user.id === id);
+
+        if (!user) {
+            return res
+                .status(404)
+                .json(`Employee with id of ${id} does not exist.`);
+        }
+
+        res.status(200).json(user);
+    } catch (error) {
+        console.error(error);
+    }
 });
 
 // POST
